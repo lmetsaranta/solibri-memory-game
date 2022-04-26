36312af834370068cc87c66c2556993f127a32cf
Adeel Shakir <adeel.shakir@live.com>
2022-04-26 16:36:58 +0300

    code enhancements

---
+++ src/data/itemList.js
@@ -0,0 +1,34 @@
+import Lennu1 from '../assets/images/Solibri_portraits_Lennu1.jpg';
+import Lennu2 from '../assets/images/Solibri_portraits_Lennu2.jpg';
+import Lennu3 from '../assets/images/Solibri_portraits_Lennu3.jpg';
+import Lennu4 from '../assets/images/Solibri_portraits_Lennu4.jpg';
+import Lennu5 from '../assets/images/Solibri_portraits_Lennu5.jpg';
+import Lennu6 from '../assets/images/Solibri_portraits_Lennu6.jpg';
+import Adeel1 from '../assets/images/Solibri_portraits_Adeel1.jpg';
+import Adeel2 from '../assets/images/Solibri_portraits_Adeel2.jpg';
+
+export const ItemList = [
+  {id: 1, imgSrc: Lennu1, status: ""},
+  {id: 1, imgSrc: Lennu1, status: ""},
+  {id: 2, imgSrc: Adeel1, status: ""},
+  {id: 2, imgSrc: Adeel1, status: ""},
+  {id: 3, imgSrc: Lennu2, status: ""},
+  {id: 3, imgSrc: Lennu2, status: ""},
+  {id: 4, imgSrc: Adeel2, status: ""},
+  {id: 4, imgSrc: Adeel2, status: ""},
+  {id: 5, imgSrc: Lennu3, status: ""},
+  {id: 5, imgSrc: Lennu3, status: ""},
+  {id: 6, imgSrc: Lennu4, status: ""},
+  {id: 6, imgSrc: Lennu4, status: ""},
+  {id: 7, imgSrc: Lennu5, status: ""},
+  {id: 7, imgSrc: Lennu5, status: ""},
+  {id: 8, imgSrc: Lennu6, status: ""},
+  {id: 8, imgSrc: Lennu6, status: ""},
+];
+
+export const ITEM_STATUS = {
+  CORRECT: 'correct',
+  WRONG: 'wrong',
+  ACTIVE: 'active',
+};
+
